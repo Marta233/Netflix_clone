e2b02d7be51555eeec72f26666b35bf6abae706b
Marta233 <martaassefa23@gmail.com>
2023-02-17 11:44:45 +0300

    ra

--- src/component/Row/Row.js
+++ src/component/Row/Row.js
@@ -12,7 +12,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl); // baseURLfromaxios + featchUrlFromapp.js(from requast)
-      console.log(request);
+      // console.log(request);
       setMovies(request.data.results);
       return request;
     }
--- src/requast.js
+++ src/requast.js
@@ -1,4 +1,5 @@
-const API_KEY = "780c98ddf017dccd9298b3225b96e2e1";
+// const API_KEY = "780c98ddf017dccd9298b3225b96e2e1";
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   featchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
